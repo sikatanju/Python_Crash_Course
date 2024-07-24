f30aca7ab49f4d87dd78f7eaa7bcacebec2ad7a0
sikatanju <saadtanju@gmail.com>
2024-07-24 10:22:12 +0600

    Added Play Button

--- 05-Projects/01-alien-invasion/settings.py
+++ 05-Projects/01-alien-invasion/settings.py
@@ -7,10 +7,16 @@ class Settings: # * A class to store all settings for Alien Invasion.
         
         # * Ship Settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # * Bullet Settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 2.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 5
+        self.bullets_allowed = 10   
+
+        # * Alien Settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 450
+        self.fleet_direction = 1
